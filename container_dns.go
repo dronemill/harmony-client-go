3ef6db2ffd339df0c2807fc27faa627067d156f0
Patrick McCarren <patrick@mmsc.io>
2015-05-11 19:31:38 -0400

    Renamed ContainerDns.go to container_dns.go

--- container_dns.go
+++ container_dns.go
@@ -1,5 +1,7 @@
 package harmonyclient
 
+import "errors"
+
 // ContainerDns holds a container's dns resource
 type ContainerDns struct {
 	ID          string `jsonapi:"name=id"`
@@ -15,5 +17,16 @@ func (c ContainerDns) GetID() string {
 // SetID to satisfy jsonapi.UnmarshalIdentifier interface
 func (c *ContainerDns) SetID(id string) error {
 	c.ID = id
+	return nil
+}
+
+// 	SetToOneReferenceID sets the reference ID and satifices jsonapi.UnmarshalToOneRelations interface
+func (c *ContainerDns) SetToOneReferenceID(name, ID string) error {
+	if name == "container" {
+		// we can skip this, becase MachineID should already be populated
+	} else {
+		return errors.New("There is no to-one relationship with name " + name)
+	}
+
 	return nil
 }
